2a1050357d70075b40054127c2b92f0f8feabbdc
Andreas <Andreas@WINDESKTOP>
2024-12-04 23:07:34 +0200

    adding benchmarks

---
+++ BenchMarker/Program.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+public class Program
+{
+	static async Task Main(string[] args)
+	{
+		// Run benchmarks
+		BenchmarkRunner.Run<BenchmarkTests>();
+	}
+
+	[MemoryDiagnoser]
+	[SimpleJob(RuntimeMoniker.Net60)]
+	[SimpleJob(RuntimeMoniker.Net80)]
+	// [SimpleJob(RuntimeMoniker.Net90)]
+	[IterationCount(10)]
+	public class BenchmarkTests
+	{
+		[Benchmark]
+		public async Task ConfigureAwaitFalseBenchmark() => await ConfigureAwait.Program.ConfigureAwaitFalse();
+
+		[Benchmark]
+		public async Task SimpleAwaitBenchmark() => await ConfigureAwait.Program.SimpleAwait();
+
+		[Benchmark]
+		public async Task ConfigureAwaitTrueBenchmark() => await ConfigureAwait.Program.ConfigureAwaitTrue();
+	}
+}
